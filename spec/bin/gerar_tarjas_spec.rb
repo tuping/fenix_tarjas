c6ae954209c1aa7412315d9f464d8f53d3906359
Rodrigo M. Albuquerque <webrodrigo@gmail.com>
2015-08-07 20:40:59 -0300

    remover debug de gerar_tarjas_spec.rb

--- spec/bin/gerar_tarjas_spec.rb
+++ spec/bin/gerar_tarjas_spec.rb
@@ -4,7 +4,6 @@ require "spec_helper"
 describe "gerar_tarjas" do
   it "deve exibir $PROGRAM_NAME e versão" do
     saida = `gerar_tarjas.rb`
-    binding.pry
     expect(saida).to include("#{FenixTarjas::VERSION}")
   end
 end
