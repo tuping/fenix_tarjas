78e6b6fb1265f9544e173d02db6c40082b1e570d
Rodrigo M. Albuquerque <webrodrigo@gmail.com>
2015-08-10 15:56:31 -0300

    teste gerar pdf

--- spec/bin/gerar_tarjas_spec.rb
+++ spec/bin/gerar_tarjas_spec.rb
@@ -8,14 +8,14 @@ describe "gerar_tarjas" do
     expect(saida).to include("#{FenixTarjas::VERSION}")
   end
 
-  it "deve gerar o pdf corretamente" do
+  it "deve gerar o pdf corretamente", :focus => true do
     hash_pdf_modelo = Digest::MD5.hexdigest(
       File.read("#{File.dirname(__FILE__)}/../fixtures/teste.pdf")
     )
-    comando = "gerar_tarjas.rb -m=1 -csv=#{File.dirname(__FILE__)}/../fixtures/teste.csv -pdf=#{File.dirname(__FILE__)}/../fixtures/tmp/teste_gerado.pdf"
-    saida = %x[comando]
+    comando = "gerar_tarjas.rb -m=1 -csv=#{File.dirname(__FILE__)}/../fixtures/teste.csv -pdf=#{File.dirname(__FILE__)}/../../tmp/teste_gerado.pdf"
+    saida = %x[#{comando}]
     hash_pdf_gerado = Digest::MD5.hexdigest(
-      File.read("#{File.dirname(__FILE__)}/../fixtures/tmp/teste_gerado.pdf")
+      File.read("#{File.dirname(__FILE__)}/../../tmp/teste_gerado.pdf")
     )
     expect(hash_pdf_gerado).to eq hash_pdf_modelo
   end
