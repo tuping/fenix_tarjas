75e558030b2088e8031c91dc64e6208a28ac516f
Rodrigo M. Albuquerque <webrodrigo@gmail.com>
2015-08-10 15:10:39 -0300

    adicionando pasta tmp

--- spec/bin/gerar_tarjas_spec.rb
+++ spec/bin/gerar_tarjas_spec.rb
@@ -1,9 +1,22 @@
 #encoding: utf-8
 require "spec_helper"
+require 'digest/md5'
 
 describe "gerar_tarjas" do
   it "deve exibir $PROGRAM_NAME e versão" do
     saida = `gerar_tarjas.rb`
     expect(saida).to include("#{FenixTarjas::VERSION}")
+  end
+
+  it "deve gerar o pdf corretamente" do
+    hash_pdf_modelo = Digest::MD5.hexdigest(
+      File.read("#{File.dirname(__FILE__)}/../fixtures/teste.pdf")
+    )
+    comando = "gerar_tarjas.rb -m=1 -csv=#{File.dirname(__FILE__)}/../fixtures/teste.csv -pdf=#{File.dirname(__FILE__)}/../fixtures/tmp/teste_gerado.pdf"
+    saida = %x[comando]
+    hash_pdf_gerado = Digest::MD5.hexdigest(
+      File.read("#{File.dirname(__FILE__)}/../fixtures/tmp/teste_gerado.pdf")
+    )
+    expect(hash_pdf_gerado).to eq hash_pdf_modelo
   end
 end
